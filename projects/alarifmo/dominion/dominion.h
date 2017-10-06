898d10fe543f64de0bbd9e7315c418a66dddb965
Mohammed Alarifi <alarifmo@flip2.engr.oregonstate.edu>
2017-10-05 19:31:58 -0700

    Assignment 2

--- projects/alarifmo/dominion/dominion.h
+++ projects/alarifmo/dominion/dominion.h
@@ -128,4 +128,12 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+
+
+/***************Five cards functions *********************/
+
+
+
+
+
 #endif
